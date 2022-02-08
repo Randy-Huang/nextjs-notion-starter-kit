6ba583caaf3dc209f71226f388ec6449684797b8
Randy Huang <randy.hunang@gmail.com>
2022-02-08 15:26:13 +0800

    Update site.config.js
    change rootNotionPageId to new one

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '64755cb12eb04de4a0798647b9cec371',
+  rootNotionPageId: '164d18a9d91e472286b390b321b2d849',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Kaihung Blog',
   domain: 'kaihunghuang.me',
   author: 'Kaihung Huang',
 
